612f4879480319bc4da2897c2ca6fa5bac9e5859
Thomas <1234328+thmsgntz@users.noreply.github.com>
2023-12-23 10:10:58 +0100

    Moving to Actix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-#[macro_use]
-extern crate rocket;
+use std::env;
+use std::io::Result;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello World!"
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey from manual hello!")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let server_addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1".to_string());
+    println!("Logging to: {server_addr}");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind((server_addr.as_str(), 8000))?
+    .run()
+    .await
 }
